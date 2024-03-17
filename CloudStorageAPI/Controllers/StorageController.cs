1fc1163774d12fbecbeb78c543d93aded9125f80
rodolfostark <rodolfojonatas@outlook.com>
2024-03-17 16:03:44 -0300

    build: setup storage controller

---
+++ CloudStorageAPI/Controllers/StorageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorageAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult UploadImage()
+    {
+        return Created();
+    }
+}
